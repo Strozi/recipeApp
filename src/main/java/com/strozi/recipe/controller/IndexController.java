fb187c22c766a95114c9b7592171898648dfc166
strozi <michal.strozynski@o2.pl>
2018-11-20 14:06:22 +0100

    adding basic spring data queries

--- src/main/java/com/strozi/recipe/controller/IndexController.java
+++ src/main/java/com/strozi/recipe/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.strozi.recipe.controller;
 
+import com.strozi.recipe.domain.Category;
+import com.strozi.recipe.domain.UnitOfMeasure;
+import com.strozi.recipe.repository.CategoryRepository;
+import com.strozi.recipe.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
-
     public String getIndexPage(){
+
         return "index";
     }
 }
--- src/main/java/com/strozi/recipe/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/strozi/recipe/repository/UnitOfMeasureRepository.java
@@ -4,8 +4,10 @@ import com.strozi.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
-
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
