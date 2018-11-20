fe3e5c76f0c10f45d63b50f78660f435aec7a759
strozi <michal.strozynski@o2.pl>
2018-11-20 14:38:31 +0100

    adding recipe service. update index controller and template

--- src/main/java/com/strozi/recipe/controller/IndexController.java
+++ src/main/java/com/strozi/recipe/controller/IndexController.java
@@ -4,7 +4,9 @@ import com.strozi.recipe.domain.Category;
 import com.strozi.recipe.domain.UnitOfMeasure;
 import com.strozi.recipe.repository.CategoryRepository;
 import com.strozi.recipe.repository.UnitOfMeasureRepository;
+import com.strozi.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,9 +14,15 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    private final RecipeService recipeService;
 
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
---
+++ src/main/java/com/strozi/recipe/service/RecipeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.strozi.recipe.service;
+
+import com.strozi.recipe.domain.Recipe;
+import com.strozi.recipe.repository.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipes = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
+        return recipes;
+    }
+}
