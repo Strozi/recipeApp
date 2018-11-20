13d72e68509bc0384f37ec100fd816aac6c02699
strozi <michal.strozynski@o2.pl>
2018-11-20 11:43:55 +0100

    adding Ingredient entity and alter Recipe

--- src/main/java/com/strozi/recipe/domain/Recipe.java
+++ src/main/java/com/strozi/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.strozi.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -15,6 +16,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     // todo add
     // private Difficulty difficulty;
     @Lob
@@ -100,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
