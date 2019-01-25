d3be42aec97285e147d285405c21e5dbfe33ba1a
strozi <michal.strozynski@o2.pl>
2019-01-25 18:31:24 +0100

    adding and testing recipe controller and view

--- src/main/java/com/strozi/recipe/service/RecipeServiceImpl.java
+++ src/main/java/com/strozi/recipe/service/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -25,5 +26,12 @@ public class RecipeServiceImpl implements RecipeService {
         return recipes;
     }
 
-
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> optionalRecipe = recipeRepository.findById(id);
+        if (!optionalRecipe.isPresent()){
+            throw new RuntimeException("Recipe Not Found!");
+        }
+        return optionalRecipe.get();
+    }
 }
